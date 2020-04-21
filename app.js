36e24cff92272e8ff59a2ee5f5c144dc9da4f3fd
sam.spaeth@gmail.com <sam.spaeth@gmail.com>
2020-04-21 16:42:41 -0500

    updated port for remote and local

--- app.js
+++ app.js
@@ -221,5 +221,5 @@ app.delete("/artentries/:id", function(req, res){
   });
 });
 
-var port = process.env.PORT || 8000 ;
-app.listen(port, function(){console.log("Server Started for prejuging app");});
+var port = process.env.PORT || 3000 ;
+app.listen(port, process.env.IP, function(){console.log("Server Started for prejuging app");});
