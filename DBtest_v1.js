83513927d995e643f95703619e16a6f00db5be70
sam.spaeth@gmail.com <sam.spaeth@gmail.com>
2020-04-28 20:55:49 -0500

    begin tests to accessdropbbox files, json coming through

---
+++ DBtest_v1.js
@@ -0,0 +1,55 @@
+const   express = require('express'),
+        bodyParser =require('body-parser'),
+        Dotenv = require('dotenv'),
+        // fetch =require('node-fetch'),
+        IsoFetch = require('isomorphic-fetch'),
+        uuid     = require('uuid/v4'),
+        Dropbox = require('dropbox').Dropbox,
+        ejs = require('ejs'),
+        app = express();
+        // import {Dropbox} from 'dropbox';
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+        
+//// DOT ENV block ////
+Dotenv.config();
+    // {path: path/filename}
+
+const port  = process.env.PORT,
+      host  = process.env.HOST,
+      DBkey = process.env.DB_ACCESS_KEY,
+      DB_API_KEY = process.env.DB_API_KEY;
+
+
+//// Get files from DB  ////
+
+const dbx = new Dropbox({
+    accessToken: DBkey,
+    fetch
+});
+
+let json  = dbx.filesListFolder({
+                        path:''
+                    }).then(res =>{
+                        console.log('res');
+                        res
+                        });
+let filesFromDB =JSON.stringify(json);
+
+let url = 'https://www.reddit.com/r/popular.json';
+
+
+app.get('/_index', (req, res, next)=>{
+    });
+
+
+app.get('/', (req, res)=>{res.redirect('/_index')});
+
+
+app.listen(port, host,console.log(`listening on port: ${port}`));
+
+
+
