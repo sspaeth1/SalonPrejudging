45603bc609eed68f92929c0630cb0e8e6b6e0dfe
sam.spaeth@gmail.com <sam.spaeth@gmail.com>
2020-08-27 18:29:56 -0400

    added groupAdmin with to view all categories

--- public/json/Groups.js
+++ public/json/Groups.js
@@ -151,6 +151,141 @@ const group7 = [
   },
 ];
 
+const groupAdmin = [
+  {
+    letter: "A1",
+    type: "Professional still media section",
+    subtype: "Didactic/Instructional ",
+    specific: "- Non-Commercial",
+  },
+  {
+    letter: "A2",
+    type: "Professional still media section",
+    subtype: "Didactic/Instructional ",
+    specific: "- Commercial",
+  },
+  {
+    letter: "B",
+    type: "Professional still media section",
+    subtype: "Editorial",
+    specific: "",
+  },
+  {
+    letter: "C",
+    type: "Professional still media section",
+    subtype: "Still Media ",
+    specific: "- Advertising and Marketing/Promotional",
+  },
+  {
+    letter: "D",
+    type: "Professional still media section",
+    subtype: "Still Media ",
+    specific: "- Medical Legal",
+  },
+  {
+    letter: "F1",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: " Didactic/Instructional – Non-Commercial",
+  },
+  {
+    letter: "F2",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: " Animation-Didactic/Instructional - Commercial",
+  },
+  {
+    letter: "F3",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: " Advertising and Marketing/Promotional",
+  },
+  {
+    letter: "F4",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: "Medical-Legal",
+  },
+  {
+    letter: "G1",
+    type: "Professional Motion Media",
+    subtype: "Interactive Media",
+    specific: " - Didactic/Instructional-Non-Commercial",
+  },
+  {
+    letter: "G2",
+    type: "Professional Motion Media",
+    subtype: "Interactive Media",
+    specific: " - Didactic/Instructional-Commercial",
+  },
+  {
+    letter: "G4",
+    type: "Professional Motion Media",
+    subtype: "Interactive Media",
+    specific: " -Gaming",
+  },
+  {
+    letter: "G5",
+    type: "Professional Motion Media",
+    subtype: "Interactive Media",
+    specific: "Interactive Media – Interactive Textbook",
+  },
+  {
+    letter: "M",
+    type: "Student Interactive Section",
+    subtype: "Interactive",
+    specific: "",
+  },
+  {
+    letter: "I1",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Anatomical/ Pathological",
+  },
+  {
+    letter: "L",
+    type: "Student Motion Media ",
+    subtype: "– Animation",
+    specific: "",
+  },
+  {
+    letter: "I2",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Surgical/Clinical Procedures",
+  },
+  {
+    letter: "J",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "- Editorial",
+  },
+  {
+    letter: "K",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Advertising and Marketing/Promotional",
+  },
+  {
+    letter: "I3",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Molecular/Biological/Life Sciences",
+  },
+  {
+    letter: "E",
+    type: "Professional still media section",
+    subtype: "Illustrated Textbook",
+    specific: " - Traditionally printed book",
+  },
+  {
+    letter: "G3",
+    type: "Professional Motion Media",
+    subtype: "Interactive Media",
+    specific: " - Advertising and Marketing/Promotional",
+  },
+];
+
 module.exports.group1 = group1;
 module.exports.group2 = group2;
 module.exports.group3 = group3;
@@ -158,3 +293,4 @@ module.exports.group4 = group4;
 module.exports.group5 = group5;
 module.exports.group6 = group6;
 module.exports.group7 = group7;
+module.exports.group7 = groupAdmin;
